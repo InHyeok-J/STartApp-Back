c0d85903c68d86bac4eb163cff012fa6bd857d7e
inhyeok <ss112d86@gmail.com>
2022-07-12 16:18:35 +0900

    chore: project init

---
+++ src/main/java/seoultech/startapp/StartappApplication.java
@@ -0,0 +1,13 @@
+package seoultech.startapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seoultech/startapp/StartappApplicationTests.java
@@ -0,0 +1,13 @@
+package seoultech.startapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
