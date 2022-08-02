c3f710f0ae527f498fab26c4034420a5b0e51ca7
inhyeok <ss112d86@gmail.com>
2022-08-02 17:04:09 +0900

    fix: register service Transactional애노테이션 추가

--- src/main/java/seoultech/startapp/event/application/EventRegisterService.java
+++ src/main/java/seoultech/startapp/event/application/EventRegisterService.java
@@ -3,6 +3,7 @@ package seoultech.startapp.event.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import seoultech.startapp.event.application.port.in.EventCommand;
 import seoultech.startapp.event.application.port.in.EventRegisterUseCase;
 import seoultech.startapp.event.application.port.out.SaveEventPort;
@@ -14,6 +15,7 @@ import seoultech.startapp.event.domain.Event;
 class EventRegisterService implements EventRegisterUseCase {
     private final SaveEventPort saveEventPort;
 
+    @Transactional
     @Override
     public void registerEvent(EventCommand eventCommand) {
 
