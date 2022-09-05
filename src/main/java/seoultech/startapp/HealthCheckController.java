2f2af757b87fbe4c3b7fe3476da640f213da2eb0
inhyeok <ss112d86@gmail.com>
2022-09-05 21:53:11 +0900

    Feat: 배포 환경 재구성

---
+++ src/main/java/seoultech/startapp/HealthCheckController.java
@@ -0,0 +1,13 @@
+package seoultech.startapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+  @GetMapping("/api/health")
+  public String healthCheck() {
+    return "ok";
+  }
+}
