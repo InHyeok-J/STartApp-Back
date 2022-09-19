b59b1bdf2d0c4260d253283a8217f684c684deea
inhyeok <ss112d86@gmail.com>
2022-09-20 01:02:44 +0900

    refactor: access log에 request body 추가

--- src/main/java/seoultech/startapp/global/config/web/LoggingFilter.java
+++ src/main/java/seoultech/startapp/global/config/web/LoggingFilter.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 import seoultech.startapp.global.common.AccessLogDto;
+import seoultech.startapp.global.common.ReusableRequestWrapper;
+import seoultech.startapp.global.util.RequestLogUtil;
 
 
 @Slf4j
@@ -19,9 +21,19 @@ public class LoggingFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-    AccessLogDto logDto = AccessLogDto.from(request);
-    log.info(objectMapper.writeValueAsString(logDto));
-    filterChain.doFilter(request, response);
+    ReusableRequestWrapper requestWrapper = new ReusableRequestWrapper(request);
+    AccessLogDto logging = logging(requestWrapper);
+    log.info(objectMapper.writeValueAsString(logging));
+    filterChain.doFilter(requestWrapper, response);
+  }
+
+  private AccessLogDto logging(ReusableRequestWrapper request) throws IOException {
+    return AccessLogDto.builder()
+        .requestMethod(request.getMethod())
+        .requestUrl(request.getRequestURI())
+        .requestIp(RequestLogUtil.getIp(request))
+        .requestBody(RequestLogUtil.getBody(request))
+        .build();
   }
 }
 
